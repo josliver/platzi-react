0703e7615400b629f9fad9018543a7a482edd1b4
Jonathan Silva <jonathan.j.silva@gmail.com>
2018-09-23 23:46:01 -0300

    Primer Componente en React

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
 // console.log('Hello world'); 
 
 
 const container = document.getElementById('app');
-const helloWorld = <h1>Hola Mundo!!!!</h1>
-ReactDOM.render(helloWorld, container);
+// const helloWorld = <h1>Hola Mundo!!!!</h1>
+ReactDOM.render(<Media />, container);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+
+class Media extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img src="" alt="" width={260} height={160} />
+                    <h3>why learn React?</h3>
+                    <p>Author</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Media;
