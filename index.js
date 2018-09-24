ffacc27cccc9675653caf024e65ba4020ed382da
Jonathan Silva <jonathan.j.silva@gmail.com>
2018-09-23 22:33:41 -0300

    Proyecto base react con react dom

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// console.log('Hello world'); 
+
+
+const container = document.getElementById('app');
+const helloWorld = <h1>Hola Mundo!!!!</h1>
+ReactDOM.render(helloWorld, container);
